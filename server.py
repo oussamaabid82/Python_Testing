fc59edd3749225a39795410c3ee6bd55cb2551ce
Oussama Abid <oussamaabidd@gmail.com>
2022-05-01 23:53:47 +0100

    ISSUE 4: mise en place des tests

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import json
 from flask import Flask, render_template, request, redirect, flash, url_for, session
 
-
 """
 Listing all data to read
 """
--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -100,3 +100,25 @@ def test_required_places_is_negative(client, first_club_fixture, second_competit
     data = response.data.decode()
     assert data.find('Please, enter a positive number') != -1
 
+"""
+Issue 4 : BUG: Clubs shouldn't be able to book more than 12 places per competition
+"""
+
+
+def test_cant_take_more_than_twelve_places(client, first_club_fixture, firt_competition_past_fixture):
+    login = client.post('/showSummary', data=dict(email=first_club_fixture['email']), follow_redirects=True)
+    assert login.status_code == 200
+
+    response = client.post(
+        '/purchasePlaces',
+        data=dict(
+            club=first_club_fixture['name'],
+            competition=firt_competition_past_fixture['name'],
+            places=13
+        )
+    )
+    assert response.status_code == 200
+    data = response.data.decode()
+    assert data.find("You can order maximum 12 places.") != -1
+
+
