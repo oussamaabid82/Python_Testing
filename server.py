5ce079e6cceae5caccb086d61273fbbbbb7deb6e
Oussama Abid <oussamaabidd@gmail.com>
2022-05-01 10:53:51 +0100

    ISSUE2: mise en palce des tests pour ISSUE 2

--- server.py
+++ server.py
@@ -56,12 +56,33 @@ def create_app(config):
 
     @app.route('/purchasePlaces',methods=['POST'])
     def purchasePlaces():
-        competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-        club = [c for c in clubs if c['name'] == request.form['club']][0]
-        placesRequired = int(request.form['places'])
-        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-        flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
+        if 'username' in session:
+            competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+            club = [c for c in clubs if c['name'] == request.form['club']][0]
+            placesRequired = int(request.form['places'])
+                       
+            # ISSUE2 : BUG: Clubs should not be able to use more than their points allowed
+            if (
+                int(placesRequired) <= int(club['points'])
+                and int(placesRequired) >= 0
+                and int(placesRequired) <= int(competition['numberOfPlaces'])
+            ):
+
+                competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+
+                flash('Great-booking complete!')
+                return render_template('welcome.html', club=club, competitions=competitions)
+
+            else:
+                # ISSUE2 : ajout d'un message en cas de nombre de place négatif
+                if int(placesRequired) < 0:
+                    flash('Please, enter a positive number')
+                else:
+                    flash("Not enough point available.")
+                return render_template('booking.html', club=club, competition=competition)
+
+        return 'You are not logged in'
+
 
 
     # TODO: Add route for points display
@@ -72,3 +93,4 @@ def create_app(config):
         return redirect(url_for('index'))
 
     return app
+    
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from server import create_app
+from ..server import create_app
 
 
 @pytest.fixture
@@ -8,4 +8,33 @@ def client():
     app = create_app({'TESTING': True})
     with app.test_client() as client:
         yield client
+
+
+"""
+Club data
+"""
+
+@pytest.fixture
+def first_club_fixture():
+    club_data = {
+        "name": "test1 GUDLFT",
+        "email": "test1_email@gudlft.com",
+        "points": "20"
+    }
+    return club_data
+
+
+"""
+competition data
+assuming past_ is first observation and post_ is second
+"""
+
+@pytest.fixture
+def firt_competition_past_fixture():
+    competition_data = {
+        "name": "competition test1",
+        "date": "2020-03-27 10:00:00",
+        "numberOfPlaces": "25"
+    }
+    return competition_data
 
